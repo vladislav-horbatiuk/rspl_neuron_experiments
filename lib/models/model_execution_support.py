5492d965463d633980207303c3683a7b145a3926
Vladyslav Horbatiuk <vshorbatiuk@gmail.com>
2023-11-12 16:18:34 +0200

    update

--- lib/models/model_execution_support.py
+++ lib/models/model_execution_support.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from lib.models.potentially_recurrent_forecaster import PRForecaster
 from lib.models.recurrent_forecaster_with_correction_block import RecurrentForecasterWithCorrectionBlock
 
@@ -37,25 +39,27 @@ def run_mod_with_correction_block_on_inputs_with_targets(
         model_mode: int = PRForecaster.MODE_TRAIN,
         *forward_args,
         **forward_kwargs
-) -> torch.Tensor:
+) -> Tuple[torch.Tensor, torch.Tensor]:
     num_samples, num_sequences = inputs.shape[:2]
     forecasts = torch.zeros(num_samples, num_sequences, 1, dtype=inputs.dtype, device=inputs.device)
     corrections = torch.zeros(num_samples, num_sequences, 1, dtype=inputs.dtype, device=inputs.device)
     previous_baseline_errors = torch.zeros(num_sequences, 1, dtype=inputs.dtype, device=inputs.device)
     previous_actual_errors = torch.zeros(num_sequences, 1, dtype=inputs.dtype, device=inputs.device)
+    prev_correction = torch.zeros(num_sequences, 1, dtype=inputs.dtype, device=inputs.device)
     if setup_ctxs:
         model.init_context(num_sequences, device=inputs.device, dtype=inputs.dtype)
     model.set_mode(model_mode)
     for i, (inp, target) in enumerate(zip(inputs, targets)):
         model.set_sample_index(i)
-        baseline_forecast, correction = model(inp, previous_baseline_errors, previous_actual_errors,
-                                              *forward_args, **forward_kwargs)
-        full_forecast = baseline_forecast + correction
+        baseline_forecast, curr_correction = model(inp, previous_baseline_errors.detach(), previous_actual_errors.detach(),
+                                                   prev_correction.detach(), *forward_args, **forward_kwargs)
+        full_forecast = baseline_forecast + curr_correction
         forecasts[i, :] = full_forecast
-        corrections[i, :] = correction
+        corrections[i, :] = curr_correction
         with torch.no_grad():
             previous_baseline_errors = target - baseline_forecast
             previous_actual_errors = target - full_forecast
+            prev_correction = curr_correction
     if delete_ctxs:
         model.delete_context()
     return forecasts, corrections
--- lib/models/recurrent_forecaster_with_correction_block.py
+++ lib/models/recurrent_forecaster_with_correction_block.py
@@ -3,6 +3,7 @@ from lib.utils.recurrent_contexts_manager import RecurrentContextsManager
 
 from torch.nn.parameter import Parameter
 import torch
+import torch.nn as nn
 
 from typing import Any
 from typing import Optional
@@ -14,12 +15,18 @@ class RecurrentForecasterWithCorrectionBlock(PRForecaster):
                  ctx_manager: RecurrentContextsManager,
                  baseline_model: PRForecaster,
                  correction_block: PRForecaster,
+                 correction_scaling: str,
+                 inp_len: int,
                  *args, **kwargs):
         super().__init__(ctx_manager, *args, **kwargs)
         self.baseline = baseline_model
         self.corrector = correction_block
-        self.correction_scale_b = Parameter(data=torch.tensor(-1.))
-        self.correction_scale_w = Parameter(data=torch.tensor(1.))
+        self.correction_scaling = correction_scaling
+        if correction_scaling == 'constrained':
+            self.correction_scale_b = Parameter(data=torch.tensor(-1.))
+            self.correction_scale_w = Parameter(data=torch.tensor(1.))
+        elif correction_scaling == 'unconstrained':
+            self.correction_scaler = nn.Sequential(nn.Linear(inp_len + 4, 1), nn.Sigmoid())
 
     def init_context(self, batch_size: int, device: Any, dtype=torch.float32, init_with: Any = 'zeros') -> None:
         self.baseline.init_context(batch_size, device, dtype, init_with)
@@ -50,11 +57,21 @@ class RecurrentForecasterWithCorrectionBlock(PRForecaster):
                 baseline_forecast = self.baseline.forward(inp)
         else:
             baseline_forecast = self.baseline.forward(inp)
-        previous_baseline_errors, previous_actual_errors = args[:2]
-        correction = self.corrector(torch.cat((inp, previous_baseline_errors,
-                                               previous_actual_errors, baseline_forecast), dim=-1))
-        correction_sigmoid_exponent = (self.correction_scale_b +
-                                       torch.abs(self.correction_scale_w) * torch.abs(previous_baseline_errors))
-        correction_scale = 1. / (1 + torch.exp(-correction_sigmoid_exponent))
+        previous_baseline_errors, previous_actual_errors, prev_correction = args[:3]
+        full_inp = torch.cat((inp, previous_baseline_errors, previous_actual_errors, prev_correction,
+                              baseline_forecast), dim=-1)
+        correction = self.corrector(full_inp)
+        # TODO: scale based on previous baseline error and actual error + correction value as well - the smaller the
+        #  actual error (given correction was big enough) - the better job our corrector did/is doing
+        if self.correction_scaling == 'constrained':
+            correction_sigmoid_exponent = (self.correction_scale_b +
+                                           torch.abs(self.correction_scale_w) * torch.abs(previous_baseline_errors))
+            correction_scale = 1. / (1 + torch.exp(-correction_sigmoid_exponent))
+        elif self.correction_scaling == 'unconstrained':
+            corrector_inp = torch.cat((inp, torch.abs(previous_baseline_errors), torch.abs(previous_actual_errors),
+                                       prev_correction, baseline_forecast), dim=-1)
+            correction_scale = self.correction_scaler(corrector_inp)
+        else:
+            correction_scale = 1.0
         correction = correction_scale * correction
         return (baseline_forecast, correction), None
